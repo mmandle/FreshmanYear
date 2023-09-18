11e2457752ff0b3da3c6af2be371f3347c79bf20
Michael Mandle <98764394+mmandle@users.noreply.github.com>
2023-09-18 18:44:27 -0400

    Update GalleryApp.java

--- cs1302/gallery/src/main/java/cs1302/gallery/GalleryApp.java
+++ cs1302/gallery/src/main/java/cs1302/gallery/GalleryApp.java
@@ -1,1 +1,75 @@
-
+   package cs1302.gallery;
+   
+   import java.net.http.HttpClient;
+   
+   import javafx.application.Application;
+   import javafx.application.Platform;
+   import javafx.scene.layout.HBox;
+   import javafx.scene.Scene;
+   import javafx.stage.Stage;
+  
+  import com.google.gson.Gson;
+  import com.google.gson.GsonBuilder;
+  
+  /**                                                                                                                                                    
+   * Represents an iTunes Gallery App.                                                                                                                   
+   */
+  public class GalleryApp extends Application {
+  
+      /** HTTP client. */
+      public static final HttpClient HTTP_CLIENT = HttpClient.newBuilder()
+          .version(HttpClient.Version.HTTP_2)           // uses HTTP protocol version 2 where possible                                                   
+          .followRedirects(HttpClient.Redirect.NORMAL)  // always redirects, except from HTTPS to HTTP                                                   
+          .build();                                     // builds and returns a HttpClient object                                                        
+  
+      /** Google {@code Gson} object for parsing JSON-formatted strings. */
+      public static Gson GSON = new GsonBuilder()
+          .setPrettyPrinting()                          // enable nice output when printing                                                              
+          .create();                                    // builds and returns a Gson object                                                              
+  
+      private Stage stage;
+      private Scene scene;
+      private HBox root;
+  
+      private GalleryLoader galLoad;
+  
+      /**                                                                                                                                                
+       * Constructs a {@code GalleryApp} object}.                                                                                                        
+       */
+      public GalleryApp() {
+          this.stage = null;
+          this.scene = null;
+          this.root = new HBox();
+  
+          galLoad = new GalleryLoader();
+      } // GalleryApp                                                                                                                                    
+  
+      /** {@inheritDoc} */
+      @Override
+      public void init() {
+          this.root.getChildren().addAll(galLoad);
+  
+          System.out.println("init() called");
+      } // init                                                                                                                                          
+  
+      /** {@inheritDoc} */
+      @Override
+      public void start(Stage stage) {
+          this.stage = stage;
+          this.scene = new Scene(this.root);
+          this.stage.setOnCloseRequest(event -> Platform.exit());
+          this.stage.setTitle("GalleryApp!");
+          this.stage.setScene(this.scene);
+          this.stage.sizeToScene();
+          this.stage.show();
+          Platform.runLater(() -> this.stage.setResizable(false));
+      } // start                                                                                                                                         
+  
+      /** {@inheritDoc} */
+      @Override
+      public void stop() {
+          // feel free to modify this method                                                                                                             
+          System.out.println("stop() called");
+      } // stop                                                                                                                                          
+  
+  } // GalleryApp  
